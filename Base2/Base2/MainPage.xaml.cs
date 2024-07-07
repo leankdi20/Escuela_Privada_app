703e5c24eec1a13875e1d8a987e88ce7a079f987
Varela Leandro <98416431+leankdi20@users.noreply.github.com>
2024-07-07 12:36:36 -0600

    Nuevo proyecto

---
+++ Base2/Base2/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Base2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btn_ok.Clicked += Btn_ok_Clicked;
+        }
+
+        private void Btn_ok_Clicked(object sender, EventArgs e)
+        {
+            lbl03.Text = "Leandro Emanuel Varela";
+
+            lbl01.Text = "Hola" + txt_01.Text;
+
+            valores.Text = dp.Date.ToString() + "" + SValor.Value + "";
+
+
+            if (rbCafe.IsChecked) valores.Text = "Bebida Cafe";
+
+            if (rbChocolate.IsChecked) valores.Text = "Bebida Chocolate";
+
+            if (rbAg.IsChecked) valores.Text = "Bebida Agua";
+
+
+
+        }
+    }
+}
